9a0af398590ad078aab505c82744170df159f21d
Khant Naing Set (Eren) <khantnaingset@outlook.com>
2024-06-07 19:16:35 +0700

    Dev (#8)
    * change route name
    
    * change route name
    
    * fix error
    
    * change route name (#3)
    
    Co-authored-by: Khant <apple@Apples-MacBook-Pro.local>
    
    * Features/kan 3 change route name (#4)
    
    * change route name
    
    * change route name
    
    * change route name
    
    * add ci
    
    * update ci workflow
    
    * update ci workflow
    
    * update ci workflow
    
    * update ci workflow
    
    * update ci workflow
    
    * update ci workflow
    
    * update ci workflow
    
    ---------
    
    Co-authored-by: Khant <apple@Apples-MacBook-Pro.local>
    
    * change color to black (#5)
    
    Co-authored-by: Khant <apple@Apples-MacBook-Pro.local>
    
    * add unit test (#7)
    
    Co-authored-by: Khant <apple@Apples-MacBook-Pro.local>
    
    ---------
    
    Co-authored-by: Khant <0e0yvsrz8vcipk9zggybwx7ehj2pdd@bots.bitbucket.org>
    Co-authored-by: Khant <apple@Apples-MacBook-Pro.local>
    Co-authored-by: Kamshein <148892958+Kamshein@users.noreply.github.com>

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::middleware('auth:api')->group(function () {
-  Route::post('/object', [ItemController::class, 'store']);
-  Route::get('/object/get_all_object', [ItemController::class, 'getAllRecords']);
+  Route::post('/object/get_one', [ItemController::class, 'store']);
+  Route::get('/objects/get_all_record_records', [ItemController::class, 'getAllRecords']);
   Route::get('/object/{key}', [ItemController::class, 'show']);
 });
